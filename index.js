90fd5d6a3ea5c7016bc41863a09c0e47f21682cb
Zain970 <itszain20@gmail.com>
2025-11-01 21:49:44 +0500

    added /users api

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ const PORT = 3000;
 
 
 App.get("/",(req,res)=>{
-    res.send("Hello world")
+    res.send("Hello world");
 })
 
+App.get("/users",(req,res)=>{
+    res.status(200).json({
+        status:'success',
+        message:"Get all users"
+    })
+})
 App.listen(PORT,()=>{
     console.log(`Server listening on port : ${PORT} `)
 })
