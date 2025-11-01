16e3dab655552899569adb9b027da1ca75150ad9
Zain970 <itszain20@gmail.com>
2025-11-01 18:56:11 +0500

    added test endpoint

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const App = express()
 
 const PORT = 3000;
 
+
+App.get("/",(req,res)=>{
+    res.send("Hello world")
+})
+
 App.listen(PORT,()=>{
     console.log(`Server listening on port : ${PORT} `)
 })
